40f0bc5d62f1c3944c8b8b2d23979435ea652801
Kialan Pillay <kialanpillay@gmail.com>
2020-08-21 11:11:29 +0200

    Added Home page component

---
+++ app/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "../App.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>iCemLab</h1>
+      </div>
+    );
+  }
+}
