7538a7c48203156f72db16baaf86fbe9288bad69
Elphas <elphas@house.me>
2020-08-28 09:55:35 +0200

    Implemented PNG export service

--- app/src/components/Diagram.js
+++ app/src/components/Diagram.js
@@ -25,7 +25,10 @@ import {
   mxPoint,
   mxPerimeter,
   mxCompactTreeLayout,
-  mxCodec
+  mxCodec,
+  mxXmlCanvas2D,
+  mxImageExport,
+  mxXmlRequest
 } from "mxgraph-js";
 
 //Diagramming Tool Component
@@ -401,6 +404,7 @@ export default class Diagram extends Component {
     );
 
     toolbar.appendChild(mxUtils.button("Export", function () {
+      // save/download XML
       const encoder = new mxCodec();
       const result = encoder.encode(graph.getModel());
       const xml = mxUtils.getXml(result);
@@ -418,6 +422,25 @@ export default class Diagram extends Component {
         element.click();
         document.body.removeChild(element);
       }
+
+      // export PNG
+      var xmlDoc = mxUtils.createXmlDocument();
+      var root = xmlDoc.createElement('output');
+      xmlDoc.appendChild(root);
+      
+      var xmlCanvas = new mxXmlCanvas2D(root);
+      var imgExport = new mxImageExport();
+      imgExport.drawState(graph.getView().getState(graph.model.root), xmlCanvas);
+      
+      var bounds = graph.getGraphBounds();
+      var w = Math.ceil(bounds.x + bounds.width);
+      var h = Math.ceil(bounds.y + bounds.height);
+      
+      var xmlText = mxUtils.getXml(root);
+      new mxXmlRequest('https://icemlab-export.herokuapp.com/', 'format=png&w=' + w +
+           '&h=' + h + '&bg=#F9F7ED&xml=' + encodeURIComponent(xmlText))
+           .simulate(document, '_blank');
+
     }));
   };
 
